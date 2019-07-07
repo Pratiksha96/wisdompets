20887d404c671b37b5e2523c61d7cbf1bd398b97
Pratiksha96 <jainpratiksha2110@gmail.com>
2019-07-07 22:12:19 +0530

    Added URLs to app.

---
+++ adoptions/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+	return HttpResponse('<p>Home view</p>')
+
+def pet_detail(request,id):
+	return HttpResponse('<p>Pet detail view with id %s </p>' %id)
