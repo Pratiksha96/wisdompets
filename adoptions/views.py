4cc89f74fbe22799d7e03350f78649beb299ecae
Pratiksha96 <jainpratiksha2110@gmail.com>
2019-07-12 23:47:53 +0530

    Added templates to view pet details.

--- adoptions/views.py
+++ adoptions/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
+
+from .models import Pet
 
 def home(request):
-	return HttpResponse('<p>Home view</p>')
+	pets = Pet.objects.all()
+	return render(request,'home.html',{'pets':pets})
 
 def pet_detail(request,id):
-	return HttpResponse('<p>Pet detail view with id %s </p>' %id)
+	try:
+		pet = Pet.objects.get(id=id)
+	except Pet.DoesNotExist:
+		raise Http404('Pet not found')
+	return render(request,'pet_detail.html',{'pet':pet})
